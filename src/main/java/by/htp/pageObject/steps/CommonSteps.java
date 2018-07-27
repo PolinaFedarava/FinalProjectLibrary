6365592f2c6f68cbe1d95bdb1557efff61571e55
Polina Fedarava <poli2105@bk.ru>
2018-07-27 15:10:35 +0300

    first commit main pages

--- src/main/java/by/htp/pageObject/steps/CommonSteps.java
+++ src/main/java/by/htp/pageObject/steps/CommonSteps.java
@@ -1,5 +1,18 @@
 package by.htp.pageObject.steps;
 
-public class CommonSteps {
+import org.openqa.selenium.WebDriver;
 
+import by.htp.pageObject.driverSingleton.WebDriverSingleton;
+
+public class CommonSteps {
+	
+	private WebDriver driver;
+
+	public void initBrowser() {
+		driver = WebDriverSingleton.getDriver();
+	}
+
+	public void closeDriver() {
+		WebDriverSingleton.closeDriver();
+	}
 }
